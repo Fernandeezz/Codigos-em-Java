1fcab5b2f361bda6e66feb2c8e34bfddee01e21e
Fernanda Fernandes do Nascimento <106761954+Fernandeezz@users.noreply.github.com>
2022-07-14 13:31:24 -0300

    Create Programa_Dominó.java
    Resolução do Desafio 1 - Bootcamp Santander.

---
+++ Programa_Dominó.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class DIO {
+
+	public static void main(String[] args) {
+		 Scanner leitor = new Scanner(System.in);
+      int n = leitor.nextInt();
+   System.out.println((n+1)*(n+2)/2);
+
+    }
+
+ }
