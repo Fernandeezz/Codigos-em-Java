8cdb8f430faf26a0f7b299eee17ba15dfa5d2869
Fernanda Fernandes do Nascimento <106761954+Fernandeezz@users.noreply.github.com>
2022-07-14 13:46:33 -0300

    Update Programa_Dominó.java

--- Programa_Dominó.java
+++ Programa_Dominó.java
@@ -3,7 +3,8 @@ O tradicional jogo de dominó possui 28 peças, sendo que ada peça está dividi
 O jogo é chamado também de duplo-6 porque esse é o maior número de círculos que aparece num quadrado de uma peça.
 
 A figura junto desse desafio mostra uma forma de organizar as 28 peças do jogo em 7 linhas. 
-Essa figura permite ver claramente quantas peças haveria num jogo de dominó, por exemplo, do tipo duplo-4: seriam todas as peças das 5 primeiras linhas, 15 peças no total. Também poderíamos ver, seguindo o padrão da figura, quantas peças possui o jogo de dominó conhecido como mexicano, que é o duplo-12. Seriam 91 peças, correspondendo a 13 linhas.
+Essa figura permite ver claramente quantas peças haveria num jogo de dominó, por exemplo, do tipo duplo-4: seriam todas as peças das 5 primeiras linhas, 15 peças no total. 
+Também poderíamos ver, seguindo o padrão da figura, quantas peças possui o jogo de dominó conhecido como mexicano, que é o duplo-12. Seriam 91 peças, correspondendo a 13 linhas.
 
 Não de forma surpreendente, existe uma fórmula com a qual podemos calcular facilmente o número de peças de um jogo do tipo duplo-N, para um número N natural qualquer: 
 ((N+1)*(N+2))/2. 
@@ -12,7 +13,6 @@ use esta fórmula para calcular e imprimir quantas peças existem num jogo de do
 /*
 
 import java.util.Scanner;
-
 public class DIO {
 
 	public static void main(String[] args) {
